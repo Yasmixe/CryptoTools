297866fe734b3519663c147718f410c656f0fce2
Lumos <88787992+Yasmixe@users.noreply.github.com>
2023-10-22 17:41:14 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,48 @@
 <?php
 //fonction cesar
 include 'connect.php';
+
+/*-------------------------------------------------------------------------------------------------*/
+
+function pgcd($a, $b) {
+    while ($b != 0) {
+        $temp = $a % $b;
+        $a = $b;
+        $b = $temp;
+    }
+    return $a;
+}
+
+function mod_inverse($a, $n) {
+    for ($x = 1; $x < $n; $x++) {
+        if (($a * $x) % $n == 1) {
+            return $x;
+        }
+    }
+    return null;
+}
+
+function chiffrement_affine($message, $a, $b) {
+    $message_chiffre = "";
+    $n = 26;  // Taille de l'alphabet
+
+    if (pgcd($a, $n) != 1) {
+        throw new Exception("La clé 'a' doit être choisie de telle sorte que pgcd(a, 26) = 1");
+    }
+
+    for ($i = 0; $i < strlen($message); $i++) {
+        $char = $message[$i];
+        if (ctype_alpha($char)) {  // Vérifiez si la lettre est alphabétique
+            $decalage = (ctype_upper($char)) ? 65 : 97;
+            $lettre_chiffree = chr((($a * (ord($char) - $decalage) + $b) % $n) + $decalage);
+            $message_chiffre .= $lettre_chiffree;
+        } else {
+            $message_chiffre .= $char;  // On ne chiffre pas les symboles et caractères spéciaux
+        }
+    }
+    return $message_chiffre;
+}
+/****************************************************************************************************************************** */
 function encryptCesar($text, $shift) {
   $result = "";
   for ($i = 0; $i < strlen($text); $i++) {
@@ -50,9 +92,6 @@ function chiffre_miroir_et_cesar($phrase) {
     return implode(" ", $result);
 }
 
-// Example usage:
-
-
 
 session_start();
 $sender = isset($_SESSION['username']) ? $_SESSION['username'] : 'Unknown Sender';
@@ -63,9 +102,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $encryptionType = $_POST["types"];
     $message = $_POST["message"];
     $key = $_POST["cle"];
-    print($key);
-    print($message);
-    
+    $a = $_POST['a'];
+    $b = $_POST['b'];
+  
+    $keyy = $a.$b;
     // Traitez le message en fonction du type de cryptage
     switch ($encryptionType) {
         case "cesar":
@@ -73,13 +113,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             break;
         case "miroir":
           $encryptedMessage = chiffre_miroir_et_cesar($message);
+            break;
+        case "affine":
+          $encryptedMessage = chiffrement_affine($message, $a, $b);
+          $sql = "INSERT INTO messages (`type_chiffrement`, `key`, `message`, `sender`, `receiver`) 
+            VALUES ('$encryptionType', '$keyy', '$encryptedMessage', '$sender', '$contact')";
+   
 
             break;
-        case "Affine":
-            // Mettez ici le code de déchiffrement Affine
-            break;
         default:
-            // Gérez le cas où le type de cryptage n'est pas reconnu
             break;
     }
     
@@ -99,13 +141,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Insérez les données dans la table `messages`
     
     $sql = "INSERT INTO messages (`type_chiffrement`, `key`, `message`, `sender`, `receiver`) 
-            VALUES ('$encryptionType', '$key', '$encryptedMessage', '$sender', '$contact')";
+            VALUES ('$encryptionType', '$keyy', '$encryptedMessage', '$sender', '$contact')";
    
     if ($conn->query($sql) === TRUE) {
         echo "Le message a été enregistré avec succès dans la base de données.";
     } else {
         echo "Erreur : " . $sql . "<br>" . $conn->error;
-    }
+    } 
     
     // Fermez la connexion à la base de données
     $conn->close();
@@ -138,36 +180,64 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     <div class="contact-section">
     <form action="index.php" method="post">
-    <div>
-      
-        <label for="contacts">Select a contact:</label>
-        <select id="contacts" name="contacts">
-            <option value="contact1">Contact 1</option>
-            <option value="contact2">Contact 2</option>
-            <option value="contact3">Contact 3</option>
-        </select>
-    </div>
-    <form action="index.php" method="post">
-    <div>
-        <label for="types">Choose encryption type:</label>
-        <select id="types" name="types">
-            <option value="cesar">Cesar</option>
-            <option value="miroir">Miroir</option>
-            <option value="affine">Affine</option>
-        </select>
-    </div>
-    <div>
-        <label for="cle">Choose a key:</label>
-        <input type="number" id="cle" name="cle" min="1" max="10">
-    </div>
-    <div>
-        <label for="message">Your Message:</label>
-        <textarea name="message" rows="5" required></textarea>
-    </div>
-    <div>
-        <input type="submit" value="Send">
-    </div>
-</form>
+        <div>
+            <label for="contacts">Select a contact:</label>
+            <select id="contacts" name="contacts">
+                <option value="contact1">Contact 1</option>
+                <option value="contact2">Contact 2</option>
+                <option value="contact3">Contact 3</option>
+            </select>
+        </div>
+        <div>
+            <label for="types">Choose encryption type:</label>
+            <form action="index.php" method="post">
+            <select id="types" name="types">
+                <option value="cesar">Cesar</option>
+                <option value="miroir">Miroir</option>
+                <option value="affine">Affine</option>
+            </select>
+        </div>
+        <form action="index.php" method="post">
+        <div id="cesar-key" style="display: none;">
+            <label for="cle">Enter the Cesar key:</label>
+            <input type="number" id="cle" name="cle" min="1" max="10">
+        </div>
+        <form action="index.php" method="post">
+        <div id="affine-params" style="display: none;">
+            <label for="a">Enter parameter 'a' (a > 0):</label>
+            <input type="text" id="a" name="a">
+            <label for="b">Enter parameter 'b' (b > 0):</label>
+            <input type="text" id="b" name="b">
+        </div>
+        <div>
+            <label for="message">Your Message:</label>
+            <textarea name="message" rows="5" required></textarea>
+        </div>
+        <div>
+            <input type="submit" value="Send">
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript">
+    document.getElementById("types").addEventListener("change", function () {
+        var selectedOption = this.value;
+        var cesarKey = document.getElementById("cesar-key");
+        var affineParams = document.getElementById("affine-params");
+
+        if (selectedOption === "cesar") {
+            cesarKey.style.display = "block";
+            affineParams.style.display = "none";
+        } else if (selectedOption === "affine") {
+            cesarKey.style.display = "none";
+            affineParams.style.display = "block";
+        } else {
+            cesarKey.style.display = "none";
+            affineParams.style.display = "none";
+        }
+    });
+</script>
+
 
         
       </div>
